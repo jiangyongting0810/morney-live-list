3321f2e0c7b45ce31f349c1ae847ecee802e6e24
YongTing <jiangyongting0810@gmail.com>
2022-07-12 22:01:44 +0800

    active-class的使用

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,15 +1,15 @@
 <template>
   <nav>
     <!--相当于a标签，下面切换的时候上面会变 -->
-    <router-link to="/money" class="item">
+    <router-link to="/money" class="item" active-class="selected">
       <Icon name="money"/>
       记账
     </router-link>
-    <router-link to="/labels" class="item">
+    <router-link to="/labels" class="item" active-class="selected">
       <Icon name="labels"/>
       标签
     </router-link>
-    <router-link to="/statistics" class="item">
+    <router-link to="/statistics" class="item" active-class="selected">
       <Icon name="statistics"/>
       统计
     </router-link>
@@ -28,8 +28,9 @@ export default {
 //nav ,nav里面有item
 nav {
   display: flex;
-  box-shadow: 0 0 3px rgba(0,0,0,0.25);
+  box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
   flex-direction: row;
+
   > .item {
     padding: 2px 0;
     width: 33.3%;
@@ -37,10 +38,15 @@ nav {
     justify-content: center;
     align-items: center;
     flex-direction: column;
-    .icon{
+
+    .icon {
       width: 32px;
       height: 32px;
+
     }
+  }
+  > .item.selected{
+    color: blue;
   }
 }
 </style>
