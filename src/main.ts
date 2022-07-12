b7a9706259a02fcf84fe1d3970cf238475954337
YongTing <jiangyongting0810@gmail.com>
2022-07-12 20:30:29 +0800

    创建<ICON\>

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,14 @@ import store from './store';
 //全局引入Nav
 import Nav from '@/components/Nav.vue';
 import Layout from '@/components/Layout.vue';
+import Icon from '@/components/Icon.vue';
 
 Vue.config.productionTip = false;
 
 //全局引入Nav
 Vue.component('Nav', Nav);
 Vue.component('Layout', Layout);
+Vue.component('Icon', Icon);
 
 new Vue({
     router,
