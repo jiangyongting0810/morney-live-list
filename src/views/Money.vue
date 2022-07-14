883f2347d578c189c934d1284a41cd4fe5ee3db0
YongTing <jiangyongting0810@gmail.com>
2022-07-14 19:19:43 +0800

    html和css写好了

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -1,29 +1,7 @@
 <template>
   <div>
-    <Layout>
-      <div class="tags">
-        <ol class="current">
-          <li>衣</li>
-          <li>食</li>
-          <li>住</li>
-          <li>行</li>
-        </ol>
-        <div class="new">
-          <button>新增</button>
-        </div>
-      </div>
-      <div>
-        <label class="notes">
-          <span class="name">备注</span>
-          <input type="text">
-        </label>
-      </div>
-      <div>
-        <ul class="types">
-          <li class="selected">支出</li>
-          <li>收入</li>
-        </ul>
-      </div>
+    <Layout class-prefix="layout">
+
       <div class="numberPad">
         <div class="output">100</div>
         <div class="buttons">
@@ -38,10 +16,36 @@
           <button>7</button>
           <button>8</button>
           <button>9</button>
-          <button>OK</button>
-          <button>0</button>
+          <button class="ok">OK</button>
+          <button class="zero">0</button>
           <button>.</button>
         </div>
+      </div>
+
+      <div>
+        <ul class="types">
+          <li class="selected">支出</li>
+          <li>收入</li>
+        </ul>
+      </div>
+
+      <div>
+        <label class="notes">
+          <span class="name">备注</span>
+          <input type="text" placeholder="在这输入备注">
+        </label>
+      </div>
+
+      <div class="tags">
+        <div class="new">
+          <button>新增标签</button>
+        </div>
+        <ul class="current">
+          <li>衣</li>
+          <li>食</li>
+          <li>住</li>
+          <li>行</li>
+        </ul>
       </div>
     </Layout>
   </div>
@@ -53,3 +57,133 @@ export default {
 };
 </script>
 
+
+<style lang="scss">
+.layout-content {
+  border: 1px solid red;
+  display: flex;
+  flex-direction: column-reverse;
+}
+</style>
+<style lang="scss" scoped>
+@import "~@/assets/style/helper.scss";
+
+.numberPad {
+  .output {
+    font-size: 36px;
+    font-family: Consolas monospace;
+    padding: 9px 16px;
+    text-align: right;
+    box-shadow: inset 0 5px 5px -3px fade-out(black, 0.9);
+  }
+
+  .buttons {
+    @extend %clearfix;
+
+    > button {
+      width: 25%;
+      height: 64px;
+      float: left;
+      //background: transparent;
+      border: none;
+      background: #f5f5f5;
+
+      &.ok {
+        height: 64*2px;
+        float: right;
+      }
+
+      &.zero {
+        width: 25*2%;
+      }
+    }
+
+  }
+}
+
+.types {
+  background: rgba(12, 153, 255, 1);
+  display: flex;
+  text-align: center;
+  font-size: 24px;
+
+  > li {
+    width: 50%;
+    height: 64px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+    color: white;
+
+    &.selected {
+      &.selected::after {
+        content: "";
+        position: absolute;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        height: 4px;
+        background: rgba(255, 255, 255, 0.6);
+      }
+    }
+  }
+}
+
+.tags {
+  //占满全部
+  flex-grow: 1;
+  font-size: 14px;
+  padding: 16px;
+  display: flex;
+  flex-direction: column-reverse;
+
+  > .current {
+    display: flex;
+    flex-wrap: wrap;
+
+    > li {
+      background: rgba(12, 153, 255, 1);
+      $h: 24px;
+      height: $h;
+      border-radius: 18px;
+      padding: 0 18px;
+      margin-right: 12px;
+      color: white;
+      line-height: $h;
+      margin-top: 4px;
+    }
+  }
+
+  > .new {
+    padding-top: 16px;
+
+    button {
+      background: transparent;
+      border: none;
+      border-bottom: 1px solid;
+      color: #999;
+    }
+  }
+}
+
+.notes {
+  font-size: 14px;
+  background: #f5f5f5;
+  padding-left: 16px;
+  display: flex;
+  align-items: center;
+
+  .name {
+    padding-right: 16px;
+  }
+
+  input {
+    height: 64px;
+    flex-grow: 1;
+    background: transparent;
+    border: none;
+    padding: 8px;
+  }
+}
+</style>
