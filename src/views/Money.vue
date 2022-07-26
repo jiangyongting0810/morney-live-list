7570b7b57772a5fb716b35e179ab9a071253fd18
YongTing <jiangyongting0810@gmail.com>
2022-07-26 22:42:05 +0800

    删除 Types.vue

--- src/views/Money.vue
+++ src/views/Money.vue
@@ -2,7 +2,6 @@
   <div>
     <Layout class-prefix="layout">
       <NumberPad @update:value="onUpdateAmount" @submit="saveRecord"/>
-      <Types :value.sync="record.type"/>
       <Tabs :data-source="recordTypeList"
             :value.sync="record.type"
       />
@@ -21,15 +20,15 @@
 <script lang="ts">
 import Vue from 'vue';
 import NumberPad from '@/components/Money/NumberPad.vue';
-import Types from '@/components/Money/Types.vue';
 import FormItem from '@/components/Money/FormItem.vue';
 import Tags from '@/components/Money/Tags.vue';
 import {Component} from 'vue-property-decorator';
 import recordTypeList from '@/constants/recordTypeList';
+import Tabs from '@/components/Tabs.vue';
 
 
 @Component({
-  components: {FormItem, Tags, Types, NumberPad},
+  components: {FormItem, Tags, NumberPad,Tabs},
 })
 
 export default class Money extends Vue {
--- src/views/Statistics.vue
+++ src/views/Statistics.vue
@@ -8,14 +8,13 @@
 </template>
 
 <script lang="ts">
-import Types from '@/components/Money/Types.vue';
 import {Component} from 'vue-property-decorator';
 import Vue from 'vue';
 import Tabs from '@/components/Tabs.vue';
 import intervalList from '@/constants/intervalList';
 import recordTypeList from '@/constants/recordTypeList';
 
-@Component({components: {Tabs, Types}})
+@Component({components: {Tabs}})
 export default class Statistics extends Vue {
   type = '-';
   interval = 'day';
